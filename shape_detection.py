b339a03e06e670e48854cfbfa2dc961a3a4e3b07
Sahir Noor Ali <sahir.noorali@gmail.com>
2018-10-22 21:42:41 +0500

    Update shape_detection.py

--- shape_detection.py
+++ shape_detection.py
@@ -1,7 +1,7 @@
 ###############################################
 # Author: Sahir                               #
 # Code:   Detecting Shapes from a Noisy Image #
-##############################################
+###############################################
 
 #Import the Libraries
 import numpy as np
@@ -11,7 +11,7 @@ import random
 #Reading the noisy image
 img = cv2.imread("fuzzy.png",1)
 
-#Displaying it to see how it looks
+#Displaying to see how it looks
 cv2.imshow("Original",img)
 
 #Converting the image to Gray Scale
@@ -20,7 +20,7 @@ gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 #Removing Gaussian Noise
 blur = cv2.GaussianBlur(gray, (3,3),0)
 
-#Applying inverse binary due to white background and adapting thresholding for better result
+#Applying inverse binary due to white background and adapting thresholding for better results
 thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 205, 1)
 
 #Checking to see how it looks
@@ -37,7 +37,7 @@ filtered = []
 
 #Looping over all found contours
 for c in contours:
-        #If it has significant area, add to list
+	#If it has significant area, add to list
 	if cv2.contourArea(c) < 1000:continue
 	filtered.append(c)
 
@@ -49,13 +49,13 @@ objects = np.zeros([img.shape[0],img.shape[1],3], 'uint8')
 
 #Looping over filtered contours
 for c in filtered:
-        #Select a random color to draw the contour
+	#Select a random color to draw the contour
 	col = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 	#Draw the contour on the image with above color
 	cv2.drawContours(objects,[c], -1, col, -1)
-        #Fetch contour area
+	#Fetch contour area
 	area = cv2.contourArea(c)
-        #Fetch the perimeter
+	#Fetch the perimeter
 	p = cv2.arcLength(c,True)
 	print(area,p)
 
